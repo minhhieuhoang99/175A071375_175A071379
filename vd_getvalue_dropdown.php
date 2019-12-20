b59038deca3d4a53b109bb7cf782790f84ededd6
minhhieuhoang99 <57648053+minhhieuhoang99@users.noreply.github.com>
2019-12-20 22:04:09 +0700

    update sua loi

--- vd_getvalue_dropdown.php
+++ vd_getvalue_dropdown.php
@@ -15,20 +15,20 @@ $connect = mysqli_connect($hostname, $username, $password, $databaseName);
 
 // mysql select query
 $query = "SELECT * FROM `users`";
-
+$query1 = "SELECT `userid` FROM `users`";
 // for method 1
 
 $result1 = mysqli_query($connect, $query);
 
 // for method 2
 
-$result2 = mysqli_query($connect, $query);
+$result2 = mysqli_query($connect, $query1);
 
 $options = "";
 
 while($row2 = mysqli_fetch_array($result2))
 {
-    $options = $options."<option>$row2[1]</option>";
+    $options = $options."<option>$row2[0]</option>";
 }
 
 ?>
